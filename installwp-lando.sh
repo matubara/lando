8c33ed5bc6832dadcd73389417f5429e149100d0
matubara <matsubara.h@gmail.com>
2022-12-06 22:32:19 +0900

    新規追加
    WPバックアップデータからリストアする
    .htaccessのドキュメントルート設置と、wp-config.phpのPREFIX変更を手動でする必要あり

---
+++ installwp-lando.sh
@@ -0,0 +1,34 @@
+# wp-config.phpのTABLE PREFIXを変更する
+# .htaccessファイルをドキュメントルートに設置する
+name=$1
+
+ pushd ./${name}/webroot
+ pwd
+ cp  ../../../archives/20221203_200039* ./
+ 
+ sed -i "s/\'wp_\'/\'bz_\'/g" ./wp-config.php
+#read -p "Press [Enter] key to move on to the next."
+
+ lando wp db import ./20221203_200039_MYSQL_DATABASE.sql
+#read -p "Press [Enter] key to move on to the next."
+ lando wp search-replace  "https://blogdeoshiete.com" "https://${name}-app.lndo.site"
+ lando wp search-replace  "http://blogdeoshiete.com" "https://${name}-app.lndo.site"
+ lando wp search-replace  "blogdeoshiete.com" "${name}-app.lndo.site"
+#read -p "Press [Enter] key to move on to the next."
+
+ SOURCECODE_UPLOADS=20221203_200039_WP_UPLOADS.tar.gz
+ SOURCECODE_PLUGINS=20221203_200039_WP_PLUGINS.tar.gz
+ SOURCECODE_THEMES=20221203_200039_WP_THEMES.tar.gz
+ tar zxvf ./${SOURCECODE_UPLOADS}  > /dev/null 2>&1 
+ tar zxvf ./${SOURCECODE_PLUGINS}  > /dev/null 2>&1 
+ tar zxvf ./${SOURCECODE_THEMES}   > /dev/null 2>&1 
+#read -p "Press [Enter] key to move on to the next."
+ cp -rp ./uploads ./wp-content 
+ cp -rp ./plugins ./wp-content 
+ cp -rp ./themes ./wp-content 
+#read -p "Press [Enter] key to move on to the next."
+ rm -rf ./uploads
+ rm -rf ./plugins 
+ rm -rf ./themes 
+
+ popd
