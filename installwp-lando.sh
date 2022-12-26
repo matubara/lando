701a4dd78583a4cfd01ebd6e597486745a5d9b5a
matubara <matsubara.h@gmail.com>
2022-12-27 01:32:28 +0900

    機能強化
    リストア対象ドメイン名とプレフィックス（バックアップファイルのタイムスタンプ）を変数に定義した。
    ※最終的には引数化する

--- installwp-lando.sh
+++ installwp-lando.sh
@@ -1,10 +1,13 @@
 # wp-config.phpのTABLE PREFIXを変更する
 # .htaccessファイルをドキュメントルートに設置する
 name=$1
+src_domain=drupal10.xyz
+src_prefix=20221227_010525
 
  pushd ./${name}/webroot
  pwd
- cp  ../../../archives/20221203_200039* ./
+ #cp  ../../../archives/20221203_200039* ./
+ cp  ../../../archives/${src_prefix}* ./
  sed -i s/\'wp_\'/\'bz_\'/g ./wp-config.php
 
  #install .htaccess file 
@@ -13,16 +16,15 @@ name=$1
 
 #read -p "Press [Enter] key to move on to the next."
 
- lando wp db import ./20221203_200039_MYSQL_DATABASE.sql
+ lando wp db import ./${src_prefix}_MYSQL_DATABASE.sql
 #read -p "Press [Enter] key to move on to the next."
- lando wp search-replace  "https://blogdeoshiete.com" "https://${name}-app.lndo.site"
- lando wp search-replace  "http://blogdeoshiete.com" "https://${name}-app.lndo.site"
- lando wp search-replace  "blogdeoshiete.com" "${name}-app.lndo.site"
+ lando wp search-replace  "${src_domain}" "${name}-app.lndo.site"
+ lando wp search-replace  "http://${name}-app.lndo.site" "https://${name}-app.lndo.site"
 #read -p "Press [Enter] key to move on to the next."
 
- SOURCECODE_UPLOADS=20221203_200039_WP_UPLOADS.tar.gz
- SOURCECODE_PLUGINS=20221203_200039_WP_PLUGINS.tar.gz
- SOURCECODE_THEMES=20221203_200039_WP_THEMES.tar.gz
+ SOURCECODE_UPLOADS=${src_prefix}_WP_UPLOADS.tar.gz
+ SOURCECODE_PLUGINS=${src_prefix}_WP_PLUGINS.tar.gz
+ SOURCECODE_THEMES=${src_prefix}_WP_THEMES.tar.gz
  tar zxvf ./${SOURCECODE_UPLOADS}  > /dev/null 2>&1 
  tar zxvf ./${SOURCECODE_PLUGINS}  > /dev/null 2>&1 
  tar zxvf ./${SOURCECODE_THEMES}   > /dev/null 2>&1 
