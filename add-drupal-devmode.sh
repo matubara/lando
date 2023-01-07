3c290ae6a498241359c250012960c5a617bf5a5c
matubara <matsubara.h@gmail.com>
2023-01-08 03:00:26 +0900

    DRUPALデバッグモードを追加
    .lando.ymlにDRUPALデバッグモードを追加

---
+++ add-drupal-devmode.sh
@@ -0,0 +1,22 @@
+# wp-config.phpのTABLE PREFIXを変更する
+# .htaccessファイルをドキュメントルートに設置する
+name=$1
+
+ pushd ./${name}
+ pwd
+
+echo `ls ./.lando_conf/drupal9/settings.local.php`
+
+#デバッグ環境の設定
+sudo cp -f  ./web/sites/default/settings.php ./web/sites/default/settings.php.bak
+sudo cp -f ./.lando_conf/drupal9/settings.local.php ./web/sites/default/
+sudo cp -f ./.lando_conf/drupal9/development.services.yml ./web/sites/
+
+sudo chmod 777 ./web/sites/default/settings.php
+
+sudo echo "" >> ./web/sites/default/settings.php
+sudo echo "if (file_exists(\$app_root . '/' . \$site_path . '/settings.local.php')) {" >> ./web/sites/default/settings.php
+sudo echo "  include \$app_root . '/' . \$site_path . '/settings.local.php';" >> ./web/sites/default/settings.php
+sudo echo "}" >> ./web/sites/default/settings.php
+
+popd
