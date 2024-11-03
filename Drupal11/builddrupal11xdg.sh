980d48b7535094ec141ef30fd85926dca10c81fd
matubara <matsubara.h@gmail.com>
2024-11-03 21:02:37 +0900

    機能追加

--- Drupal11/builddrupal11xdg.sh
+++ Drupal11/builddrupal11xdg.sh
@@ -12,6 +12,9 @@ elif [ $# -eq 1 ]; then
     name=$1
     #デフォルト値設定
     DRUPALSET=drupal10
+elif [ $# -eq 2 ] && [ $2 = "drupal10+" ]; then
+    name=$1
+    DRUPALSET=$2
 elif [ $# -eq 2 ] && [ $2 = "drupal10" ]; then
     name=$1
     DRUPALSET=$2
@@ -22,6 +25,13 @@ else
     echo "引数が不正です"
     exit 1
 fi
+
+if [ -d ./${name} ]; then
+  echo "【警告】既にフォルダが存在します。";
+  echo "処理を中断します";
+  exit 1;
+fi
+
 ######################################################
     echo "【確認】設定内容を確認してください"
     echo "フォルダ名: ${name}"
@@ -32,6 +42,11 @@ fi
 #constファイルの読み込み
 source ./const_lando_builddrupal.sh
 ######################################################
+
+TIME1=$(cat /proc/uptime | awk '{print $1}')
+echo time1: $TIME1
+
+##### 計測したい処理ここから
 
 
 echo "${YELLOW}Drupal開発環境を構築します${RESET}"
@@ -117,5 +132,16 @@ echo "${YELLOW}アプリをインストールします${RESET}"
 fi
 
 lando info
+
+
+##### 計測したい処理ここまで
+
+TIME2=$(cat /proc/uptime | awk '{print $1}')
+echo time2: $TIME2
+
+DIFF=$(echo "$TIME2 - $TIME1" | bc)
+
+echo "${GREEN}経過時間(s)：${DIFF}${RESET}"
+
 
 echo "${YELLOW}すべての処理を完了しました。${RESET}" 
